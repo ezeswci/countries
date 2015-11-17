83353056519075413e37279fb319477ad57b53a8
ezeswci <ezequiel@swci.com.ar>
2015-11-17 16:26:35 -0300

    veamos

--- js/chequeoConexion.js
+++ js/chequeoConexion.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function checkConnection() {
 var state = navigator.connection.type;
-/*if (state == window.Connection.NONE)
+if (state == window.Connection.NONE)
 {
     // doesn't have internet, notify
 	return false;
@@ -10,25 +10,23 @@ else
 {
     // has internet, continue work accessing internet
 	return true;
-}*/
-	return true;// Prueba interna 
+}
+	/*return true;// Prueba interna */
     //alert('Connection type: ' + states[networkState]);
 }
 
 function checkConnectionControl() {
-var state = navigator.connection.type;
-alert(state);
-alert(state.join('\n'));
-if (state == window.Connection.NONE)
-{
-    // doesn't have internet, notify
-	alert("Sin");
-}
-else
-{
-    // has internet, continue work accessing internet
-	alert("Con");
-}
-	return true;// Prueba interna 
-    //alert('Connection type: ' + states[networkState]);
+var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
 }
