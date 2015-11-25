b79f23f1c7c05581f999807ddd26dd264087f153
ezeswci <ezequiel@swci.com.ar>
2015-11-24 23:42:15 -0300

    alertas

--- js/notifications.js
+++ js/notifications.js
@@ -3,6 +3,7 @@
 function mensajes(){
 	alert("Entra mensajes");
 	setTimeout(function(){alerta();},10000);
+	setTimeout(function(){alertas();},20000);
 	}
 function alertDismissed() {
     // do something
@@ -12,5 +13,12 @@ function alerta(){
 cordova.plugins.notification.local.schedule({
     title: "New Message",
     message: "Hi, are you ready? We are waiting."
+});
+}
+function alertas(){
+window.plugin.notification.local.add({
+    id:         1,
+    message:    'I love BlackBerry!',
+    json:       JSON.stringify({ test: 123 })
 });
 }
