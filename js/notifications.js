08b98938522974463ed3b307a44bfb0cf9c8bb4e
ezeswci <ezequiel@swci.com.ar>
2015-11-25 10:11:04 -0300

    vamos q va

--- js/notifications.js
+++ js/notifications.js
@@ -2,18 +2,12 @@
 	setTimeout(function(){mensajes();},10000);
 function mensajes(){
 	alert("Entra mensajes");
-	setTimeout(function(){alerta();},10000);
+	setTimeout(function(){alertas();},10000);
 	setTimeout(function(){alertas();},20000);
 	}
 function alertDismissed() {
     // do something
 	alert("Dismissed");
-}
-function alerta(){
-cordova.plugins.notification.local.schedule({
-    title: "New Message",
-    message: "Hi, are you ready? We are waiting."
-});
 }
 function alertas(){
 window.plugin.notification.local.add({
